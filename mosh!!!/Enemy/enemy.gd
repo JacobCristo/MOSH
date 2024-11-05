207aed2b4f1fb438720006a7ad8a12e0965a3ce8
JacobCristo <jpc4259@g.rit.edu>
2024-11-05 13:18:14 -0500

    changed main sprites and gave them walk/idle anims

--- mosh!!!/Enemy/enemy.gd
+++ mosh!!!/Enemy/enemy.gd
@@ -3,9 +3,15 @@ extends CharacterBody2D
 @export var movement_speed = 20.0
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
 	#direction to the player
 	var direction = global_position.direction_to(player.global_position)
+	if direction.x < 0:
+		sprite.flip_h = true
+	else: 
+		sprite.flip_h = false
+		
 	velocity = direction * movement_speed
 	move_and_slide()
--- mosh!!!/Player/player.gd
+++ mosh!!!/Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var movement_speed = 40.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
 	movement()
@@ -12,4 +13,12 @@ func movement():
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
 	var mov = Vector2(x_mov, y_mov)
 	velocity = mov.normalized() * movement_speed
+	if Input.is_action_pressed("right") or Input.is_action_pressed("left") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+		sprite.play("walk")
+		if x_mov < 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
+	else:
+		sprite.play("idle")
 	move_and_slide()
